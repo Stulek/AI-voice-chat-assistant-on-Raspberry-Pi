5005d3961c1aa55e1a9eca1afec14dac6d3ab976
Atex Stulek <125310322+Stulek@users.noreply.github.com>
2025-09-03 13:05:43 +0200

    Create autostart.sh

---
+++ autostart.sh
@@ -0,0 +1,7 @@
+mkdir -p ~/.config/autostart && cat <<EOF > ~/.config/autostart/chromium-chatgpt.desktop
+[Desktop Entry]
+Type=Application
+Name=Chromium
+Exec=/usr/bin/chromium-browser --new-window --disable-infobars --autoplay-policy=no-user-gesture-required https://chatgpt.com
+X-GNOME-Autostart-enabled=true
+EOF
